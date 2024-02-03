ad5ee7f914424917cf5ee6c97e49c82b4ae93f2b
mohamedeliwa <mohamed.av11@gmail.com>
2024-02-03 23:53:28 +0200

    added a db connection health check endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('healthz')
+  async healthz() {
+    return await this.appService.healthz();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@nestjs/common';
-
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 @Injectable()
 export class AppService {
+  constructor(private prisma: PrismaService) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async healthz() {
+    try {
+      await this.prisma.$queryRaw`SELECT 1 + 1`;
+      return 'Connection has been established successfully.';
+    } catch (error) {
+      console.error('Unable to connect to the database:', error);
+      throw new BadRequestException('Unable to connect to the database');
+    }
   }
 }
