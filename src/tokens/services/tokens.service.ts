aba3c45004a3feb34c5ed4e5615055b8f692e12d
mohamedeliwa <mohamed.av11@gmail.com>
2024-02-11 12:03:15 +0200

    added services to describe token model and create new tokens

--- src/tokens/services/tokens.service.ts
+++ src/tokens/services/tokens.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateTokenDto } from '../dtos/create.token.dto';
+import { Token } from '@prisma/client';
 
 @Injectable()
-export class TokensService {}
+export class TokensService {
+  constructor(private prisma: PrismaService) {}
+
+  /**
+   * @returns the description of the token model
+   */
+  async describe() {
+    return this.prisma.token.fields;
+  }
+
+  /**
+   * creates a new token
+   * @param createtokenDto token's data to be created
+   * @returns the newly created token object
+   */
+  async create(createTokenDto: CreateTokenDto): Promise<Token> {
+    const { decisionId, ...rest } = createTokenDto;
+
+    // creating a token and connecting it to a decision by id
+    const token = this.prisma.token.create({
+      data: {
+        ...rest,
+        decision: {
+          connect: { id: decisionId },
+        },
+      },
+    });
+    return token;
+  }
+}
--- src/tokens/tokens.module.ts
+++ src/tokens/tokens.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TokensController } from './controllers/tokens.controller';
 import { TokensService } from './services/tokens.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [TokensController],
   providers: [TokensService],
 })
